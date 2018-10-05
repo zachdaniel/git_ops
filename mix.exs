b1d674989e207f587e22f0e96f93eb9c022c7836
zachdaniel <zachary.s.daniel@gmail.com>
2018-10-05 15:28:12 -0400

    chore: Set up CI tools

--- mix.exs
+++ mix.exs
@@ -8,11 +8,51 @@ defmodule GitOps.MixProject do
       app: :git_ops,
       version: @version,
       elixir: "~> 1.7",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
+      name: "Git Ops",
+      docs: docs(),
+      source_url: "https://github.com/spandex-project/spandex",
       deps: deps(),
-      dialyzer: [plt_add_apps: [:mix]]
+      dialyzer: [plt_add_apps: [:mix]],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls.travis": :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
+
+  defp description() do
+    """
+    A tool for managing the version and changelog of a project using conventional commits.
+    """
+  end
+
+  defp package() do
+    [
+      name: :git_ops,
+      maintainers: "Zachary Daniel",
+      licenses: ["MIT License"],
+      links: %{
+        "GitHub" => "https://github.com/zachdaniel/git_ops"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -26,7 +66,10 @@ defmodule GitOps.MixProject do
     [
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.6", only: :test},
       {:git_cli, "~> 0.2"},
+      {:inch_ex, "~> 0.5", only: [:dev, :test]},
       {:nimble_parsec, "~> 0.2"}
     ]
   end
