34cbb2efb759ee6b0be3a380f4afea3d8e596660
Tom Large <tomplarge@gmail.com>
2018-11-28 14:08:32 -0600

    feat: dry_run release option

--- test/changelog_test.exs
+++ test/changelog_test.exs
@@ -3,33 +3,7 @@ defmodule GitOps.Test.ChangelogTest do
 
   alias GitOps.Changelog
 
-  test "initialize with existing changelog raises" do
-    changelog = "./TEST_CHANGELOG.md"
-
-    File.write!(changelog, "")
-
-    try do
-      assert_raise RuntimeError, ~r/File already exists:/, fn ->
-        Changelog.initialize(changelog)
-      end
-    after
-      File.rm!(changelog)
-    end
-  end
-
-  test "initialize creates non-empty changelog file" do
-    changelog = "./TEST_CHANGELOG.md"
-
-    try do
-      Changelog.initialize(changelog)
-
-      assert File.read!(changelog) != ""
-    after
-      File.rm!(changelog)
-    end
-  end
-
-  test "writing commits to changefile works correctly" do
+  setup do
     changelog = "./TEST_CHANGELOG.md"
 
     commits = [
@@ -51,19 +25,50 @@ defmodule GitOps.Test.ChangelogTest do
       }
     ]
 
-    try do
+    on_exit(fn -> File.rm!(changelog) end)
+
+    %{changelog: changelog, commits: commits}
+  end
+
+  test "initialize with existing changelog raises", context do
+    changelog = context.changelog
+
+    File.write!(changelog, "")
+
+    assert_raise RuntimeError, ~r/File already exists:/, fn ->
       Changelog.initialize(changelog)
-
-      old_content =
-        changelog
-        |> File.read!()
-        |> String.length()
-
-      Changelog.write(changelog, commits, "0.1.0", "0.2.0")
-
-      assert String.length(File.read!(changelog)) > old_content
-    after
-      File.rm!(changelog)
     end
+  end
+
+  test "initialize creates non-empty changelog file", context do
+    changelog = context.changelog
+
+    Changelog.initialize(changelog)
+
+    assert File.read!(changelog) != ""
+  end
+
+  test "writing commits to changefile works correctly", context do
+    changelog = context.changelog
+
+    Changelog.initialize(changelog)
+
+    changes = Changelog.write(changelog, context.commits, "0.1.0", "0.2.0")
+
+    assert String.length(changes) > 0
+  end
+
+  test "writing with dry_run produces changes that aren't written", context do
+    changelog = context.changelog
+
+    Changelog.initialize(changelog)
+
+    original_contents = File.read!(changelog)
+
+    changes = Changelog.write(changelog, context.commits, "0.1.0", "0.2.0", dry_run: true)
+
+    assert String.length(changes) > 0
+
+    assert File.read!(changelog) == original_contents
   end
 end
