3d778b6334d956b54a4dff09d2b98feeb8d67fb7
Marina Krasnova <marinakrasnovatr81@gmail.com>
2019-01-22 14:42:16 +0200

    Allow empty prefix (#2)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,23 @@ See [Conventional Commits](Https://conventionalcommits.org) for commit guideline
 
 <!-- changelog -->
 
+## [v0.6.0](https://github.com/zachdaniel/git_ops/compare/0.5.0...v0.6.0) (2019-1-22)
+
+
+
+
+### Features:
+
+* test: add tests for version_tag_prefix
+
+* dry_run release option
+
+### Bug Fixes:
+
+* allow no prfexix
+
+* initial mix project check (#6)
+
 ## [v0.5.0](https://github.com/zachdaniel/git_ops/compare/0.4.1...v0.5.0) (2018-11-20)
 
 
--- test/release_test.exs
+++ test/release_test.exs
@@ -6,25 +6,74 @@ defmodule GitOps.Mix.Tasks.Test.ReleaseTest do
 
   alias Mix.Tasks.GitOps.Release
 
-  setup do
-    changelog = "TEST_CHANGELOG.md"
+  describe "with version_tag_prefix" do
+    setup do
+      changelog = "TEST_CHANGELOG.md"
 
-    Application.put_env(:git_ops, :mix_project, GitOps.MixProject)
-    Application.put_env(:git_ops, :repository_url, "repo/url.git")
-    Application.put_env(:git_ops, :manage_mix_version?, true)
-    Application.put_env(:git_ops, :changelog_file, changelog)
-    Application.put_env(:git_ops, :manage_readme_version, true)
-    Application.put_env(:git_ops, :types, custom: [header: "Custom"], docs: [hidden?: false])
-    Application.put_env(:git_ops, :version_tag_prefix, "v")
+      Application.put_env(:git_ops, :mix_project, GitOps.MixProject)
+      Application.put_env(:git_ops, :repository_url, "repo/url.git")
+      Application.put_env(:git_ops, :manage_mix_version?, true)
+      Application.put_env(:git_ops, :changelog_file, changelog)
+      Application.put_env(:git_ops, :manage_readme_version, true)
+      Application.put_env(:git_ops, :types, custom: [header: "Custom"], docs: [hidden?: false])
+      Application.put_env(:git_ops, :version_tag_prefix, "v")
 
-    on_exit(fn -> File.rm!(changelog) end)
+      on_exit(fn -> File.rm!(changelog) end)
 
-    %{changelog: changelog}
+      %{changelog: changelog}
+    end
+
+    test "release with dry run works properly", context do
+      File.write!(context.changelog, "")
+
+      Release.run(["--dry-run", "--force-patch"])
+    end
   end
 
-  test "release with dry run works properly", context do
-    File.write!(context.changelog, "")
+  describe "without version_tag_prefix" do
+    setup do
+      changelog = "TEST_CHANGELOG.md"
 
-    Release.run(["--dry-run", "--force-patch"])
+      Application.put_env(:git_ops, :mix_project, GitOps.MixProject)
+      Application.put_env(:git_ops, :repository_url, "repo/url.git")
+      Application.put_env(:git_ops, :manage_mix_version?, true)
+      Application.put_env(:git_ops, :changelog_file, changelog)
+      Application.put_env(:git_ops, :manage_readme_version, true)
+      Application.put_env(:git_ops, :types, custom: [header: "Custom"], docs: [hidden?: false])
+
+      on_exit(fn -> File.rm!(changelog) end)
+
+      %{changelog: changelog}
+    end
+
+    test "release with dry run works properly", context do
+      File.write!(context.changelog, "")
+
+      Release.run(["--dry-run", "--force-patch"])
+    end
+  end
+
+  describe "with empty version_tag_prefix" do
+    setup do
+      changelog = "TEST_CHANGELOG.md"
+
+      Application.put_env(:git_ops, :mix_project, GitOps.MixProject)
+      Application.put_env(:git_ops, :repository_url, "repo/url.git")
+      Application.put_env(:git_ops, :manage_mix_version?, true)
+      Application.put_env(:git_ops, :changelog_file, changelog)
+      Application.put_env(:git_ops, :manage_readme_version, true)
+      Application.put_env(:git_ops, :types, custom: [header: "Custom"], docs: [hidden?: false])
+      Application.put_env(:git_ops, :version_tag_prefix, "")
+
+      on_exit(fn -> File.rm!(changelog) end)
+
+      %{changelog: changelog}
+    end
+
+    test "release with dry run works properly", context do
+      File.write!(context.changelog, "")
+
+      Release.run(["--dry-run", "--force-patch"])
+    end
   end
 end
